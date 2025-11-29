fc9af33dddb36cd30f1d9344740b7b35d90c707c
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-29 09:39:08 -0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-BACKEND_URL = "https://ai-maksimal-production.up.railway.app"
+BACKEND_URL = "https://aimaksimal-production.up.railway.app"
 
 BOT_LOGIN_EMAIL = "Fayziev978@mail.ru"
 BOT_LOGIN_PASSWORD = "123456"
